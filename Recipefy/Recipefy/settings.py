1e7542e514fd00cec6c01671243a8417b10fcfcc
yasserkabbout <yasserkabbout@gmail.com>
2018-12-26 03:55:01 +0300

    Adding Heroku

--- Recipefy/Recipefy/settings.py
+++ Recipefy/Recipefy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # CORS Config
 CORS_ORIGIN_ALLOW_ALL = True
